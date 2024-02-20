c2bc01ac1eaed9801d33f1e5e2681aeff4570773
sagir567 <sagir567@gmail.com>
2024-02-20 22:41:43 +0200

    fixed timer UI

--- Assets/Scripts/CountdownTimer.cs
+++ Assets/Scripts/CountdownTimer.cs
@@ -8,20 +8,22 @@ public class CountdownTimer : MonoBehaviour
 {
     [SerializeField ]public string levelToLoad;
     [SerializeField]private float timer;
-    //private TextMeshProUGUI timerSecoends;
+    private Text timerSecoends;
     // Start is called before the first frame update
     void Start()
     {
-        //timerSecoends = GetComponent<TextMeshProUGUI>();   
+        timerSecoends = GetComponent<Text>();   
     }
 
     // Update is called once per frame
     void Update()
     {
         timer -= Time.deltaTime;
-        //timerSecoends.text = timer.ToString("f0");
+        timerSecoends.text = "Time: ";
+        timerSecoends.text= timerSecoends.text.Insert(timerSecoends.text.Length-1,timer.ToString("f1"));
+        Debug.Log(timerSecoends.text);
         if (timer <= 0)
-        {
+        { 
             Application.LoadLevel(levelToLoad);
         }
         
